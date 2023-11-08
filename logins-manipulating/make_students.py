3cc0b51c7517b28c2078c3614c17771af7733871
Daniel Zagaynov <kotopesutility@altlinux.org>
2023-11-08 20:46:07 +0300

    Little code fixes

--- logins-manipulating/make_students.py
+++ logins-manipulating/make_students.py
@@ -32,11 +32,11 @@ def main(arguments_list=None):
     This is the main functions for repositories manipulating
     """
 
-    if arguments_list == None:
+    if arguments_list is None:
         return 1
 
     parser = argparse.ArgumentParser(
-            description="This program register students on machine."\
+            description="This program register students on machine."
                         " It uses .csv file as input."
             )
 
@@ -44,7 +44,7 @@ def main(arguments_list=None):
         '--csv',
         dest='csv_file',
         required=True,
-        help="File name with list of students in .csv format."\
+        help="File name with list of students in .csv format."
              "Fields must be delimited by ':', and strings in \"\"."
         )
 
@@ -71,47 +71,42 @@ def main(arguments_list=None):
             help="Special comment string which describes group of students"
             )
 
-    args=parser.parse_args(arguments_list[1:])
+    args = parser.parse_args(arguments_list[1:])
 
     if os.geteuid() != 0:
-        print ("You need root permissions to execute this script")
+        print("You need root permissions to execute this script")
         return 1
 
-
     if args.passw_file == "":
-        args.passw_file="paswords_"+args.csv_file;
+        args.passw_file = "paswords_" + args.csv_file
 
-    passw_file=open(args.passw_file,"w")
+    passw_file = open(args.passw_file, "w")
     os.chmod(args.passw_file, 0o600)
     os.chown(args.passw_file, os.getuid(), os.getgid())
 
-    tmp_file_name="/tmp/make_students_%d" % os.getpid()
+    tmp_file_name = "/tmp/make_students_%d" % os.getpid()
 
-    tmp_passwd_file=open(tmp_file_name,"w")
+    tmp_passwd_file = open(tmp_file_name, "w")
     os.chmod(tmp_file_name, 0o600)
     #
     # Folowing code has no effect.
     #
-    #os.chown(tmp_file_name, os.getuid(), os.getgid())
+    # os.chown(tmp_file_name, os.getuid(), os.getgid())
 
-
-    hash_salt_file=open("/dev/random","r")
-    hash_salt=hash_salt_file.read(60)
+    hash_salt_file = open("/dev/random", "r")
+    hash_salt = hash_salt_file.read(60)
     hash_salt_file.close()
 
-
-    new_str=""
-    for i in range(0,60):
+    new_str = ""
+    for i in range(0, 60):
         new_str += chr(ord(hash_salt[i]) % (60-30) + 31)
 
-
-    my_hash=hashlib.sha1()
-    today=date.today()
-    hash_salt=today.ctime()+new_str
+    my_hash = hashlib.sha1()
+    today = date.today()
+    hash_salt = today.ctime()+new_str
     my_hash.update(hash_salt)
 
-
-    teachers=args.teachers.split(',')
+    teachers = args.teachers.split(',')
 
     for students in parse_csv(args.csv_file):
         full_name = students["name"]
@@ -125,12 +120,12 @@ def main(arguments_list=None):
 
         command_line = "/usr/sbin/useradd --user-group"\
                        " --comment \"%s,,,%s; %s\" --create-home --shell /bin/bash %s" %\
-                    (
+                       (
                         full_name,
                         email,
                         args.comment,
                         login
-                    )
+                       )
         os.system(command_line)
         #   os.system("echo passwd -e %s" % (login))
         os.system("chmod -R o-rwx ~%s" % login)
--- repos-manipulating/scripts/repos.py
+++ repos-manipulating/scripts/repos.py
@@ -6,6 +6,7 @@ import argparse
 import sys
 import getpass
 
+
 def parse_opts(config_file):
     """
     Parse options from config file
@@ -13,27 +14,28 @@ def parse_opts(config_file):
     name = value
     with comments in shell style '#'
     """
-    options=dict()
+    options = dict()
 
     #
     # set default options
     #
-    options['remote_user']=getpass.getuser()
-    options['students_logins']="/etc/students.csv"
-    options['repository_way']="/home/%STUDENT_LOGIN%/repos"
+    options['remote_user'] = getpass.getuser()
+    options['students_logins'] = "/etc/students.csv"
+    options['repository_way'] = "/home/%STUDENT_LOGIN%/repos"
 
-    file_options=open(config_file)
+    file_options = open(config_file)
     for record in file_options:
-        record=record.strip()
+        record = record.strip()
         if record != "" and record[0] == '#':
             continue
-        opt=record.split('=')
-        if len(opt) == 2 :
-            options[opt[0].strip()]=opt[1].strip(' \n')
+        opt = record.split('=')
+        if len(opt) == 2:
+            options[opt[0].strip()] = opt[1].strip(' \n')
 
     file_options.close()
 
     return options
+
 
 def parse_student_string(student):
     """
@@ -51,7 +53,7 @@ def parse_student_string(student):
     if len(student) == 0:
         return None
 
-    words=student.split(":")
+    words = student.split(":")
     if len(words) != 3:
         return None
 
@@ -67,7 +69,7 @@ def main(arguments_list=None):
     This is the main functions for repositories manipulating
     """
 
-    if arguments_list == None:
+    if arguments_list is None:
         return 1
 
     parser = argparse.ArgumentParser(
@@ -83,67 +85,61 @@ def main(arguments_list=None):
 
     parser.add_argument("--action",
                         dest="action",
-                        choices = ['clone','pull','push','add-to-trac','remove-from-trac'],
+                        choices=['clone', 'pull', 'push', 'add-to-trac', 'remove-from-trac'],
                         required=False,
                         default="pull",
                         help="Action with git repository"
                         )
 
-    args=parser.parse_args(arguments_list[1:])
+    args = parser.parse_args(arguments_list[1:])
 
-    options=parse_opts(args.config_file)
-    print (options)
+    options = parse_opts(args.config_file)
+    print(options)
 
-
-
-
-    file_d=open(options["students_logins"],"r")
-
+    file_d = open(options["students_logins"], "r")
 
     if args.action == "clone":
-        if not  os.path.exists("src"):
+        if not os.path.exists("src"):
             os.mkdir("src")
 
     for student_string in file_d:
 
-        student=parse_student_string(student_string)
-        if student == None:
+        student = parse_student_string(student_string)
+        if student is None:
             continue
 
         name  = student[0]
         login = student[2]
 
-        command_line= ""
+        command_line = ""
 
-        print ("begin work for  \"(%s) %s\" ---->" % (name, login))
+        print("begin work for  \"(%s) %s\" ---->" % (name, login))
 
         if args.action == "clone":
-
-
-            remote_path=options["repository_way"].replace("%STUDENT_LOGIN%",login)
-            command_line="git clone ssh://%s\@%s%s src/%s" %\
-                         (
+            remote_path = options["repository_way"].replace("%STUDENT_LOGIN%", login)
+            command_line = "git clone ssh://%s\@%s%s src/%s" %\
+                           (
                               options["remote_user"],
                               options["host"],
                               remote_path,
                               login
-                         )
+                           )
 
         if args.action == "pull" or args.action == "push":
-            command_line="cd src/%s; git %s; cd .." % (login, args.action)
+            command_line = "cd src/%s; git %s; cd .." % (login, args.action)
 
         if args.action == "add-to-trac":
-            command_line="trac-admin %s repository add %s-%s %s/src/%s/.git git" %\
-                    (
-                            options["trac_env_path"],
-                            options["group_prefix"],
-                            login,
-                            os.getcwd(),
-                            login
-                    )
+            command_line = "trac-admin %s repository add %s-%s %s/src/%s/.git git" %\
+                            (
+                             options["trac_env_path"],
+                             options["group_prefix"],
+                             login,
+                             os.getcwd(),
+                             login
+                            )
 
         if args.action == "remove-from-trac":
-            command_line="trac-admin %s repository remove %s-%s" %\
+            command_line = "trac-admin %s repository remove %s-%s" %\
                     (
                             options["trac_env_path"],
                             options["group_prefix"],
@@ -151,7 +147,7 @@ def main(arguments_list=None):
                     )
 
         os.system(command_line)
-        print ("finish work for \"%s\" <----\n\n" % login)
+        print("finish work for \"%s\" <----\n\n" % login)
 
     file_d.close()
 
